b86c868202bed9e6ce3604c56b68d4ce44b8c3bc
Stackbit Code Editor <projects@stackbit.com>
2022-01-20 14:56:47 +0000

    AllMenu.js: changed

--- content/pages/AllMenu.js
+++ content/pages/AllMenu.js
@@ -6,6 +6,9 @@ export default function Home() {
             <title>Menu</title>
             <meta name="myportfolioproyect" content="Menu from El Ranchito 3" />
             <link rel="icon" href="/favicon.ico" />
+            <body>
+                <embed src="files/husky.pdf" type="application/pdf" width="100%" height="100%" />
+            </body>
         </flexbox>
     );
 }
